f78f3ec470c280335cd7936fa35790cd3dd5c2c3
Aidar <a1dar.ufa@gmail.com>
2022-09-14 18:15:14 +0500

    Initial Commit

---
+++ Swiftbook-iosDev-2.2-UIKit-ColorSliders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swiftbook-iosDev-2.2-UIKit-ColorSliders
+//
+//  Created by Айдар Абдуллин on 14.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
