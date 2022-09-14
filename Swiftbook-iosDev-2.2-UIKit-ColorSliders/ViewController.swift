bbce2e408bc8e63a736614f34c1c406d36d496a2
Aidar <a1dar.ufa@gmail.com>
2022-09-14 19:02:27 +0500

    Programming ViewController

--- Swiftbook-iosDev-2.2-UIKit-ColorSliders/ViewController.swift
+++ Swiftbook-iosDev-2.2-UIKit-ColorSliders/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorLabel: UILabel!
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        colorLabel.layer.cornerRadius = 5
+        colorLabel.layer.masksToBounds = true
+        
+        redSlider.value = 1
+        redSlider.minimumValue = 1
+        redSlider.maximumValue = 255
+        greenSlider.value = 1
+        greenSlider.minimumValue = 1
+        greenSlider.maximumValue = 255
+        blueSlider.value = 1
+        blueSlider.minimumValue = 1
+        blueSlider.maximumValue = 255
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func redSlider(_ sender: UISlider) {
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+        colorLabel.backgroundColor = UIColor(red: CGFloat(redSlider.value)/255, green: CGFloat(greenSlider.value)/255, blue: CGFloat(blueSlider.value)/255, alpha: 1)
+    }
+    @IBAction func greenSlider(_ sender: UISlider) {
+        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        colorLabel.backgroundColor = UIColor(red: CGFloat(redSlider.value)/255, green: CGFloat(greenSlider.value)/255, blue: CGFloat(blueSlider.value)/255, alpha: 1)
+    }
+    @IBAction func blueSlider(_ sender: UISlider) {
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+        colorLabel.backgroundColor = UIColor(red: CGFloat(redSlider.value)/255, green: CGFloat(greenSlider.value)/255, blue: CGFloat(blueSlider.value)/255, alpha: 1)
+    }
+    
+    
 }
 
